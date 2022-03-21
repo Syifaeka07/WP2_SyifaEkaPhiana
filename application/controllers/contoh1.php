45ade55861e2344229c68098c1b8a3807b544a12
Syifaeka07 <syifaeka0709@gmail.com>
2022-03-21 15:39:40 +0700

    menambahkan contoh1.php

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index ()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo "Nama saya Syifa Eka Phiana
+            Saya tinggal di daerah Menceng 
+            Olahraga yang saya sukai adalah Bulutangkis";
+            
+    }
+}
